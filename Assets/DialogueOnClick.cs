dd3c358f9e68df57bad91fd7464bb2c76f2bb9ee
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-08-07 22:55:01 -0500

    carteles de información de biología agregados

---
+++ Assets/DialogueOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOnClick : MonoBehaviour
+{
+    public float rayDistance = 3f; // distancia máxima para "clic"
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Solo procesa clic izquierdo si no hay diálogo activo
+        if (!DialogueManager.instance.isDialogueActive && Input.GetMouseButtonDown(0))
+        {
+            // Raycast desde el centro de la pantalla (FPS)
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                // Busca si el objeto clickeado tiene DialogueTrigger
+                DialogueTrigger trigger = hit.collider.GetComponent<DialogueTrigger>();
+                if (trigger != null)
+                {
+                    // Lanza el diálogo
+                    trigger.TriggerDialogue();
+                }
+            }
+        }
+    }
+}
