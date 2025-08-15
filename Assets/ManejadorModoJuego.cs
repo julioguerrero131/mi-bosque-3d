6c0606fdd74e4f256badddfe269bd0a07163fe30
danlevil <danlevil@espol.edu.ec>
2025-08-15 00:56:52 -0500

    feat:Modo Contrarreloj v1.0.1 - conectar los botones de los modos de juego con el script cs

---
+++ Assets/ManejadorModoJuego.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManejadorModoJuego : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void activarTemporizador()
+    {
+        Debug.Log("activar timer para contrarreloj");
+    }
+    public void modoNormal()
+    {
+        Debug.Log("Desactivar timer, volviendo al modo normal");
+    }
+}
