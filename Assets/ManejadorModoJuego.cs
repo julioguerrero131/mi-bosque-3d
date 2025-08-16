ceae069da8e618683a3e48bf25bf2fcb5efc7ce7
danlevil <danlevil@espol.edu.ec>
2025-08-16 01:39:04 -0500

    feat:Modo Contrarreloj v1.0.3 - conexión entre el texto y el script cs que incrementa el timer (ascendente), activa y desactiva el recuadro del timer

--- Assets/ManejadorModoJuego.cs
+++ Assets/ManejadorModoJuego.cs
@@ -1,26 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManejadorModoJuego : MonoBehaviour
 {
+
+
+    /*public Text relojTxt;
+    public GameObject relojBox;*/
+    [SerializeField] private Text relojTxt;
+    [SerializeField] private GameObject relojBox;
+    private int minutos;
+    private float segundos;
+    public bool IsContrarreloj = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (relojBox != null)
+                relojBox.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsContrarreloj)
+        {
+            actualizarTextoContador();
+        }
     }
     public void activarTemporizador()
     {
         Debug.Log("activar timer para contrarreloj");
+        IsContrarreloj = true;
+        if (relojBox != null)
+        {
+            Debug.Log("encontré RelojBox");
+
+            relojBox.SetActive(true);
+        }
+    }
+    private void contadorMinutos()
+    {
+        segundos = segundos + Time.deltaTime;
+        if (segundos >= 60)
+        {
+            segundos = 0;
+            minutos = minutos + 1;
+
+        }
+    }
+    public void actualizarTextoContador()
+    {
+        contadorMinutos();
+        if(segundos < 9.5f)
+        {
+            relojTxt.text = minutos.ToString() + ":0" + segundos.ToString("f0");
+        }
+        else
+        {
+            relojTxt.text = minutos.ToString() + ":" + segundos.ToString("f0");
+
+        }
     }
     public void modoNormal()
     {
         Debug.Log("Desactivar timer, volviendo al modo normal");
+        IsContrarreloj = false;
+        if (relojBox != null)
+        {
+            relojBox.SetActive(false);
+        }
+        minutos = 0;
+        segundos = 0;
+        actualizarTextoContador();
     }
 }
